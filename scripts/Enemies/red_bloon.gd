1b5d2dcbd4412104a8c19de81f9f29fc31e4f4a3
ShadowWasNotAvailable <thogja16@vgs.nfk.no>
2023-11-23 11:07:41 +0100

    Remove bloon at end of path

--- scripts/Enemies/red_bloon.gd
+++ scripts/Enemies/red_bloon.gd
@@ -1,5 +1,6 @@
 extends Area2D
 var temp_global_money
+var temp_global_life = 100
 var anim
 var damaged
 var speed = 3
@@ -10,9 +11,16 @@ func _ready():
 	path_follow = get_parent()
 	anim = $AnimatedSprite2D
 	anim.play("Initial_color")
+	path_follow.set_rotates(false)
+	path_follow.set_loop(false)
 
 func _physics_process(delta):
 	path_follow.set_progress(path_follow.get_progress() + speed + delta)
+	
+	if path_follow.progress_ratio == 1.0:
+		temp_global_life -= 1
+		self.queue_free()
+
 
 
 func _on_hitbox_body_entered(body):
