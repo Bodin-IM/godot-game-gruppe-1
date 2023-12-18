9cdccff1c6ac3e274ecb5c74df2b5143c9412739
ShadowWasNotAvailable <113894475+ShadowWasNotAvailable@users.noreply.github.com>
2023-12-18 12:30:20 +0100

    Useless shit gone

--- scripts/test/wave_summon.gd
+++ scripts/test/wave_summon.gd
@@ -1,18 +1,8 @@
 extends Path2D
 
 var path
-var current_wave = -1
-var waves
 var rounds
-var current_round = -1
-var red_valuesl
-var blue_values
-var roundReady = true
-var red_sprite_frames = preload("res://Assets/bloon_sprite_frames/red_sprite_frames.tres")
-var blue_sprite_frames = preload("res://Assets/bloon_sprite_frames/blue_sprite_frames.tres")
-var bloon_scene = preload("res://scenes/enemys/Red_bloon.tscn")
 var balloons
-var testProg = 0
 
 func _ready():
 	path = get_node("Node2D.")
